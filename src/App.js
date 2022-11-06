22e864b8a5be7bc3cfe79cf5b1646893774d95c0
ramveer998 <ramveerch@student.iul.ac.in>
2022-11-06 16:55:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>hey i wa</p>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default App;
